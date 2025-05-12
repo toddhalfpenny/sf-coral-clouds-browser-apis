af38eaef2bf38c68f1846cb8d2b65ed3db0b28b4
Todd Halfpenny <todd@toddhalfpenny.com>
2025-05-12 21:20:05 +0100

    Exp Carousel images

--- cc-site/main/default/lwc/experienceHeroCarousel/experienceHeroCarousel.js
+++ cc-site/main/default/lwc/experienceHeroCarousel/experienceHeroCarousel.js
@@ -1,3 +1,55 @@
 import { LightningElement } from 'lwc';
+import expImages from '@salesforce/resourceUrl/coralcloudsite';
+const LOGTAG = "experienceHeroCarousel";
 
-export default class ExperienceHeroCarousel extends LightningElement {}
+export default class ExperienceHeroCarousel extends LightningElement {
+  isFullScreen = false;
+  landscapePopover;
+  portraitPopover;
+  expImage01 = expImages + '/exp-carousel-01.jpg';
+  expImage02 = expImages + '/exp-carousel-03.jpg';
+  expImage03 = expImages + '/exp-carousel-04.jpg';
+  expImage02Rotated = expImages + '/exp-carousel-02-rotated.jpg';
+
+  renderedCallback() {
+    this.landscapePopover = this.template.querySelector(".landscapePopover");
+    this.portraitPopover = this.template.querySelector(".portraitPopover");
+
+    console.log("Adding listener for deviceorientation");
+    // window.matchMedia("(orientation: portrait)").addEventListener("change", e => {
+    //   const portrait=e.matches;
+    //   if(portrait){ 
+    //     console.log("portrait");
+    //     }
+    //   else{ 
+    //     console.log("landscape");
+    //     }
+    // });
+  }
+
+  handleFullScreen() {
+    console.log(LOGTAG, "handleFullScreen");
+    // this.isFullScreen = !this.isFullScreen;
+
+    //TODO Check whether in landscape or potrait
+    const mql = window.matchMedia("(orientation: portrait)");
+    if (mql.matches) {
+      console.log("portrait");
+      const popupOpened = this.portraitPopover.togglePopover();
+      console.log("popupOpened", popupOpened);
+    } else {
+      console.log("landscape");
+      const popupOpened = this.landscapePopover.togglePopover();
+      console.log("popupOpened", popupOpened);
+    }
+  }
+
+  handleNonFullScreen() {
+    console.log(LOGTAG, "handleNonFullScreen");
+    this.isFullScreen = !this.isFullScreen;
+  }
+
+  handleOrientation(event) {
+    console.log("DeviceOrientation", event);
+  }
+}
